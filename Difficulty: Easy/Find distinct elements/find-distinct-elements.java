6edfb253dbf7518b294c7bf34188d6ba70b24e72
PUSHPENDRA-7 <koshtapushpendra320@gmail.com>
2025-09-03 09:52:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find distinct elements/find-distinct-elements.java
+++ Difficulty: Easy/Find distinct elements/find-distinct-elements.java
@@ -1,7 +1,6 @@
 // User function Template for Java
 class Solution {
     static int distinct(int arr[]) {
-        // code here
         HashSet<Integer> set = new HashSet<>();
         for(Integer i : arr){
             set.add(i);
